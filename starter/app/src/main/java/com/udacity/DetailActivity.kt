01208dd8a5fddcc8a5a0c9bd08b1937cccfafad4
EsraaAkram <totalappsingplay@gmail.com>
2022-10-29 21:18:48 +0200

    Update libraries replace kotlin extension (Deprecated) with data binding and add it to the activities

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,15 +1,20 @@
 package com.udacity
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_detail.*
+import androidx.databinding.DataBindingUtil
+import com.udacity.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
 
+    var binding:ActivityDetailBinding?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-        setSupportActionBar(toolbar)
+        //setContentView(R.layout.activity_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
+        setSupportActionBar(binding?.toolbar)
     }
 
 }
